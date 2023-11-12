723b94e81a4a81fb0a155457032d4e3f621d1c54
Ramiro Otero <ramirootero98@gmail.com>
2023-11-12 19:11:40 -0300

    WhatsApp

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import '../../App.css'
+import { Link } from "react-router-dom";
+import whatsApp from '../../Assets/whatsapp.png'
 const Footer = () => {
 
 
@@ -8,10 +10,11 @@ const Footer = () => {
         <div className="footer">
             <span>PROTOTYPE</span>    
             <ul className="footerList">
-                <il><a>GitHub</a></il>
-                <il><a>Linkedin</a></il>
-                <il><a>Behance</a></il>
-                <il><a>Instagram</a></il>
+                <Link to="https://www.linkedin.com/in/ramiro-joaquin-otero-frontend-js-css3-html5-graphic-designer/" target="_blank">Linkedin</Link>
+                <Link to="https://github.com/RamaOtero" target="_blank">GitHub</Link>
+                <Link to="https://www.behance.net/ramirootero1" target="_blank">Behance</Link>
+                <Link to="https://wa.me/+5492214958365" target="_blank"><img className="whatsApp" src={whatsApp} alt="WhatsApp" /></Link>
+                
             </ul>
         </div>
     )
